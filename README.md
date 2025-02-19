5f9697dcfb7d1d3803e9becd5e25d841176dd99b
frankpulido <frankpulido@me.com>
2025-02-19 08:05:57 +0100

    fixed entry route and updated readme for cloning advise

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # THE PROJECT
 
 
+# CLONING THE PROJECT AND SET UP
+
+The project was developed in Laravel framework. First clone it :
+
+git clone https://github.com/frankpulido/TaskManager-Laravel.git
+
+Then run Composer to get needed dependencies :
+
+    install composer
+
+If you are using Apache and when trying to open in browser you get a "Failed to open stream : Permission denied" error, run :
+
+    sudo chown -R daemon:daemon storage
+    sudo chown -R daemon:daemon bootstrap/cache
+
+(Those 2 commands will change the directories ownership to the web server user)
+
+
+
 # GIT FLOW
 
 1) Initialize :
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataViewController;
 use App\Http\Controllers\TaskController;
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
+Route::get('/', [DataViewController::class, 'allGrid'])->name('dataview.allgrid');
 Route::get('/dataview/allgrid', [DataViewController::class, 'allGrid'])->name('dataview.allgrid');
 Route::get('/dataview/byproject', [DataViewController::class, 'byProject'])->name('dataview.byproject');
 Route::get('/dataview/byprogress', [DataViewController::class, 'byProgress'])->name('dataview.byprogress');
